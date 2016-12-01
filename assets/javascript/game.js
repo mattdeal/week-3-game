68880c8816c47d361df032cad1988450a38ade79
mwdeal1 <mwdeal1@gmail.com>
2016-12-01 07:34:00 -0500

    Small JS change

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+
+//todo: dictionary/list of words
+
+
+document.onkeyup = function(event) {
+	// Captures the key press, converts it to lowercase, and saves it to a variable.
+	var letter = String.fromCharCode(event.keyCode).toLowerCase();
+};
